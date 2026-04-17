5c183c1704879b82dd244c137eedc65c90d2f060
Lombqq <joaquinblombardi@gmail.com>
2026-04-17 11:39:14 -0300

    ejercicio1

---
+++ Tp3 Bucles/src/tp3/bucles/Tp3Bucles.java
@@ -0,0 +1,30 @@
+
+package tp3.bucles;
+
+public class Tp3Bucles {
+
+    
+    public static void main(String[] args) {
+  
+        
+    
+        for (int a = 1; a <= 100; a++) {
+            System.out.println(a);
+        }
+
+        int b = 1;
+        while (b <= 100) {
+            System.out.println(b);
+            b++;
+        }
+
+        int c = 1;
+        do {
+            System.out.println(c);
+            c++;
+        } while (c <= 100);
+    }
+}
+
+    
+
